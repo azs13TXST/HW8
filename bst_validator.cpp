18ff8e5e547f9c5d05688153e6841928887a08b5
Albert Soliz <azs13@txstate.edu>
2020-12-01 09:28:08 +0000

    Pseudocode

--- bst_validator.cpp
+++ bst_validator.cpp
@@ -1,6 +1,39 @@
+/******************************
+Pseudocode:
+Formation function
+  Allocates origin to a new Tree
+  Sets right of origin to NULL
+  Sets left of origin to NULL
+  Sets value of origin to value
+  Returns origin
+
+Ammend function
+  If value is less than or equal to value of parent
+    Sets left of parent to the Ammend function
+    Returns parent
+  Else
+    Sets right of parent to the Ammend function
+    Returns parent
+
+BSTValidator function
+  If the parent is NULL
+    Returns false
+  If the value of parent is less than minimum and more than maximum
+    Returns false
+  Returns the BSTValidator function twice for the left and right branches
+
+main function
+  Initializes parent to NULL
+  Initializes minimum to -500
+  Initializes maximum to 500
+  Initializes bool result to false
+  Initializes int array[]
+  For loop to Ammend each parent
+  Prints the result
+******************************/
+
 #include <iostream>
 #include <vector>
-#include <string>
 
 struct Tree  {
   int value;
@@ -8,34 +41,30 @@ struct Tree  {
   Tree* left;
 };
 
-Tree* Formation(int value)  {
+Tree* Formation(Tree* origin, int value)  {
   Tree* origin = new Tree();
 
-  origin->value = value;
   origin->right = NULL;
   origin->left = NULL;
+  origin->value = value;
   
   return origin;
 }
 
 Tree* Ammend(Tree* parent, int value)  {
-  if(parent == NULL)  {
-    parent = Formation(value);
-  }
 
-  else if(value <= parent->value)  {
+  if(value <= parent->value)  {
     parent->left = Ammend(parent->left, value);
+    return parent;
   }
 
   else  {
     parent->right = Ammend(parent->right, value);
+    return parent;
   }
-
-  return parent;
 }
 
 bool BSTValidator(Tree* parent, int minimum, int maximum)  {
-  std::cout << parent->left->value << std::endl;
 
   if(parent == NULL)  {
     return false;
@@ -50,10 +79,9 @@ bool BSTValidator(Tree* parent, int minimum, int maximum)  {
 
 int main()  {
   Tree* parent = NULL;
-  int minimum = -694206669;
-  int maximum = 694206669;
+  int minimum = -500;
+  int maximum = 500;
   bool result = false;
-  
   int array[] = {10,5,15,2,7, 11, 25, 1};
 
   for (int y = 0; y < (sizeof(array)/sizeof(array[0])); y++)  {
@@ -61,5 +89,6 @@ int main()  {
   }
 
   result = BSTValidator(parent, minimum, maximum);
+  std::cout << "Is this a BST?" << std::endl;
   std::cout << result << std::endl;
 }
